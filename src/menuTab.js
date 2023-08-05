72a0804f921a6c80c2e8c3662ffd1df039152baa
High23 <warenasb2@gmail.com>
2023-08-04 22:19:25 -0700

    Creates and inputs the food menu tab information

---
+++ src/menuTab.js
@@ -0,0 +1,20 @@
+export default function addMenuTabContent() {
+    const container = document.querySelector('.container');
+
+    container.classList.remove('contact');
+    container.classList.add('menu-grid');
+    for (let i = 0; i < 9; i++) {
+        const div = document.createElement('div');
+        div.classList.add('food');
+
+        const img = document.createElement('img');
+        img.setAttribute('alt', 'Image of food')
+
+        const span = document.createElement('span');
+        span.textContent = `Food ${i + 1}`;
+
+        div.appendChild(img);
+        div.appendChild(span);
+        container.appendChild(div);
+    }
+}
